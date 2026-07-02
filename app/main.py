4b9b20b378423bdaae5316353e74742d4076fb19
f-piken <totpiken@outlook.com>
2026-07-02 23:54:19 +0700

    error terus3

--- app/main.py
+++ app/main.py
@@ -1,37 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from app.services.scheduler import SchedulerService
-
 app = FastAPI()
 
-
 class GenerateScheduleRequest(BaseModel):
-
     jamaahs: list
-
     team_leaders: list
-
     muthowifs: list
-
     departures: list
 
+@app.get("/")
+def root():
+    return {"status": "ok"}
 
 @app.post("/generate-schedule")
-def generate_schedule(
-    request: GenerateScheduleRequest
-):
+def generate_schedule(request: GenerateScheduleRequest):
+
+    from app.services.scheduler import SchedulerService
 
     result = SchedulerService.generate(
-
         jamaahs=request.jamaahs,
-
         team_leaders=request.team_leaders,
-
         muthowifs=request.muthowifs,
-
         departures=request.departures
-
     )
 
     return result
